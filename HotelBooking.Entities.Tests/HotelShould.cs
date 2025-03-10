d09bb02000ce77bfc2e1463ea500de33f240e12f
elenamoralesgil <elena.morales104@alu.ulpgc.es>
2025-03-10 11:53:39 +0000

    feat: added tests for the hotel entity

---
+++ HotelBooking.Entities.Tests/HotelShould.cs
@@ -0,0 +1,39 @@
+﻿using HotelBookingKata.Entities;
+using Shouldly;
+namespace HotelBooking.Entities.Tests;
+
+public class HotelShould
+{
+
+    Hotel Hotel;
+    [SetUp]
+    public void Setup()
+    {
+        Hotel = new Hotel("1", "Hotel");
+    }
+
+    [Test]
+    public void set_room()
+    {
+        Hotel.SetRoom("101", RoomType.Standard);
+        Hotel.Rooms.Count.ShouldBe(1);
+    }
+
+    [Test]
+    public void get_room()
+    {
+        Hotel.SetRoom("101", RoomType.Standard);
+        var room = Hotel.GetRoom("101");
+        room.Number.ShouldBe("101");
+        room.Type.ShouldBe(RoomType.Standard);
+    }
+
+    [Test]
+    public void get_rooms()
+    {
+        Hotel.SetRoom("101", RoomType.Standard);
+        Hotel.SetRoom("102", RoomType.JuniorSuite);
+        var rooms = Hotel.GetRooms();
+        rooms.Count.ShouldBe(2);
+    }
+}
